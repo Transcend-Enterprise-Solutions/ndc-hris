a1dc4549a6f14e481669313c6c789a7549d1e262
janllyod09 <bulicjanllyod@gmail.com>
2024-08-02 16:34:44 +0800

    updateszzz

--- resources/views/livewire/user/leave-application-table.blade.php
+++ resources/views/livewire/user/leave-application-table.blade.php
@@ -11,53 +11,64 @@
 
     {{-- Leave Application Table --}}
     <div class="w-full flex justify-center">
-        <div class="w-full bg-white dark:bg-gray-800 rounded-2xl p-6 shadow-md">
-            <h1 class="text-lg font-bold text-center text-black dark:text-white mb-6">Leave Application</h1>
-            <div class="overflow-x-auto">
-                <table class="min-w-full bg-white dark:bg-gray-800 overflow-hidden">
-                    <thead class="bg-gray-200 dark:bg-gray-700 rounded-xl">
-                        <tr class="whitespace-nowrap">
-                            <th scope="col" class="px-4 py-2 text-center">Date of Filing</th>
-                            <th scope="col" class="px-4 py-2 text-center">Type of Leave</th>
-                            <th scope="col" class="px-4 py-2 text-center">Details of Leave</th>
-                            <th scope="col" class="px-4 py-2 text-center">Requested Days</th>
-                            <th scope="col" class="px-4 py-2 text-center">Requested Start Date</th>
-                            <th scope="col" class="px-4 py-2 text-center">Requested End Date</th>
-                            <th scope="col" class="px-4 py-2 text-center">Approved Days</th>
-                            <th scope="col" class="px-4 py-2 text-center">Approved Start Date</th>
-                            <th scope="col" class="px-4 py-2 text-center">Approved End Date</th>
-                            <th class="px-4 py-2 text-center">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($leaveApplications as $leaveApplication)
-                        <tr class="whitespace-nowrap">
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->date_of_filing }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->type_of_leave }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->details_of_leave }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->number_of_days }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->start_date }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->end_date }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->approved_days ?? 'N/A' }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->approved_start_date ?? 'N/A' }}</td>
-                            <td class="px-4 py-2 text-center">{{ $leaveApplication->approved_end_date ?? 'N/A' }}</td>
-                            <td class="px-4 py-2 text-center">
-                                <span
-                                    class="inline-block px-3 py-1 text-sm font-semibold 
-                                {{ str_starts_with($leaveApplication->status, 'Approved') ? 'text-green-800 bg-green-200' : 
-                                (str_starts_with($leaveApplication->status, 'Disapproved') ? 'text-red-800 bg-red-200' : 'text-yellow-800 bg-yellow-200') }} rounded-full">
-                                    {{ $leaveApplication->status }}
-                                </span>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+        <div class="flex justify-center w-full">
+            <div class="w-full bg-white rounded-2xl p3 sm:p-8 shadow dark:bg-gray-800 overflow-x-visible">
+                <div class="pb-4 pt-4 sm:pt-1">
+                    <h1 class="text-lg font-bold text-center text-slate-800 dark:text-white">Leave Application</h1>
+                </div>
+                <div class="flex flex-col p-3">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block w-full py-2 align-middle">
+                            <div class="overflow-hidden border dark:border-gray-700 rounded-lg">
+                                <div class="overflow-x-auto">
+                                    <table class="w-full min-w-full">
+                                        <thead class="bg-gray-200 dark:bg-gray-700 rounded-xl">
+                                            <tr class="whitespace-nowrap">
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Date of Filing</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Type of Leave</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Details of Leave</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Requested Days</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Requested Start Date</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Requested End Date</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Approved Days</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Approved Start Date</th>
+                                                <th scope="col" class="px-5 py-3 text-sm font-medium text-left uppercase text-center">Approved End Date</th>
+                                                <th class="px-5 py-3 text-gray-100 text-sm font-medium text-center sticky right-0 z-10 bg-gray-600 dark:bg-gray-600">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($leaveApplications as $leaveApplication)
+                                            <tr class="whitespace-nowrap">
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->date_of_filing }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->type_of_leave }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->details_of_leave }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->number_of_days }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->start_date }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->end_date }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->approved_days ?? 'N/A' }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->approved_start_date ?? 'N/A' }}</td>
+                                                <td class="px-4 py-2 text-center">{{ $leaveApplication->approved_end_date ?? 'N/A' }}</td>
+                                                <td class="px-5 py-4 text-sm font-medium text-center whitespace-nowrap sticky right-0 z-10 bg-white dark:bg-gray-800">
+                                                    <span
+                                                        class="inline-block px-3 py-1 text-sm font-semibold 
+                                                    {{ str_starts_with($leaveApplication->status, 'Approved') ? 'text-green-800 bg-green-200' : 
+                                                    (str_starts_with($leaveApplication->status, 'Disapproved') ? 'text-red-800 bg-red-200' : 'text-yellow-800 bg-yellow-200') }} rounded-full">
+                                                        {{ $leaveApplication->status }}
+                                                    </span>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="p-5 border-t border-neutral-500 dark:border-neutral-200">
+                                    {{ $leaveApplications->links() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="p-5 border-t border-neutral-500 dark:border-neutral-200">
-                {{ $leaveApplications->links() }}
-            </div>
-
         </div>
     </div>
 
