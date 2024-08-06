aeb018ec8df079b98116212c1dd818d8570f0455
dale0333 <alvindalejoyosa30@gmail.com>
2024-08-06 20:32:57 +0800

    dashboards

--- resources/views/components/dashboard/dashboard-card-05.blade.php
+++ resources/views/components/dashboard/dashboard-card-05.blade.php
@@ -1,3 +1,3 @@
-<div class="flex flex-col col-span-full sm:col-span-6 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
+<div class="flex flex-col col-span-full sm:col-span-9 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
     @livewire('dashboard.dashboard-dtr')
 </div>
