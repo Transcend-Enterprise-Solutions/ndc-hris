4fe079213dca41292d481b39fd14135b94fbca91
Jhon Francis Duarte <jhamaicaduarte@gmail.com>
2024-08-07 23:05:43 +0800

    sidebar all goods

--- app/Livewire/Admin/Reports/HeadCount.php
+++ app/Livewire/Admin/Reports/HeadCount.php
@@ -2,14 +2,19 @@
 
 namespace App\Livewire\Admin\Reports;
 
+use App\Models\EmployeesDtr;
 use Livewire\Component;
 use App\Models\User;
 use Carbon\Carbon;
+use Exception;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\EmployeeReportExport;
 
 class HeadCount extends Component
 {
-    public function render()
-    {
+    public $date;
+
+    public function render(){
         $totalEmployees = User::where('user_role', 'emp')->count();
         $newEmployeesThisMonth = User::where('user_role', 'emp')
             ->whereMonth('created_at', Carbon::now()->month)
@@ -20,10 +25,56 @@ class HeadCount extends Component
             ->groupBy('department')
             ->get();
 
+        $dailyAttendance = null;
+        if ($this->date) {
+            $dailyAttendance = EmployeesDtr::where('date', $this->date)
+                            ->where(function ($query) {
+                                $query->where('remarks', 'Present')
+                                        ->orWhere('remarks', 'Late');
+                            })
+                            ->count();
+        }else{
+            $dailyAttendance = 0;
+        }
+
         return view('livewire.admin.reports.head-count',[
-           'totalEmployees' => $totalEmployees,
+            'totalEmployees' => $totalEmployees,
             'newEmployeesThisMonth' => $newEmployeesThisMonth,
             'departmentCounts' => $departmentCounts,
+            'dailyAttendance' => $dailyAttendance,
         ]);
+    }
+
+    public function exportTotalEmployee(){
+        try{
+            $filters = null;
+            return Excel::download(new EmployeeReportExport($filters), 'EmployeesList.xlsx');
+        }catch(Exception $e){
+            throw $e;
+        }
+    }
+
+    public function exportTotalEmployeeThisMonth(){
+        try{
+
+        }catch(Exception $e){
+            throw $e;
+        }
+    }
+
+    public function exportTotalEmployeeInDepartment(){
+        try{
+
+        }catch(Exception $e){
+            throw $e;
+        }
+    }
+
+    public function exportTotalEmployeeDaily(){
+        try{
+
+        }catch(Exception $e){
+            throw $e;
+        }
     }
 }
--- resources/views/livewire/admin/reports/head-count.blade.php
+++ resources/views/livewire/admin/reports/head-count.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="flex flex-col col-span-full sm:col-span-full bg-white dark:bg-slate-800 shadow-lg rounded-lg border border-slate-200 dark:border-slate-700">
+    class="flex flex-col col-span-full sm:col-span-full bg-white dark:bg-slate-800 rounded-lg border border-slate-200 dark:border-slate-700">
     <div class="px-5 pt-5">
         <header class="flex justify-between items-start mb-2">
             <h2 class="text-lg font-semibold text-slate-800 dark:text-slate-100">Head Count Report</h2>
@@ -19,30 +19,103 @@
     </div>
     
     <div class="p-4">
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div class="grid grid-cols-12 gap-4">
+
             <!-- Total Employees Card -->
-            <div class="bg-blue-100 dark:bg-blue-800 p-4 rounded-lg shadow">
-                <h3 class="text-lg font-semibold text-blue-800 dark:text-blue-200">Total Employees</h3>
-                <p class="text-3xl font-bold text-blue-600 dark:text-blue-300">{{ $totalEmployees }}</p>
+            <div class="col-span-full sm:col-span-6 bg-blue-100 dark:bg-blue-800 p-4 rounded-lg shadow flex justify-between">
+                <div>
+                    <h3 class="text-sm font-semibold text-blue-800 dark:text-gray-200">Total Employees</h3>
+                    <p class="text-3xl font-bold text-blue-600 dark:text-gray-200">{{ $totalEmployees }}</p>
+                </div>
+
+                <!-- Export to Excel -->
+                <div class="w-1/5 sm:w-auto flex justify-center items-center h-full">
+                    <button wire:click="exportTotalEmployee" 
+                        class="inline-flex items-center focus:outline-none"
+                        type="button" title="Export to Excel">
+                        <img class="flex dark:hidden" src="/images/export-excel.png" width="22" alt="">
+                        <img class="hidden dark:block" src="/images/export-excel-dark.png" width="22" alt="">
+                    </button>
+                </div>
             </div>
             
             <!-- New Employees This Month Card -->
-            <div class="bg-green-100 dark:bg-green-800 p-4 rounded-lg shadow">
-                <h3 class="text-lg font-semibold text-green-800 dark:text-green-200">New Employees This Month</h3>
-                <p class="text-3xl font-bold text-green-600 dark:text-green-300">{{ $newEmployeesThisMonth }}</p>
+            <div class="col-span-full sm:col-span-6 bg-yellow-100 dark:bg-yellow-800 p-4 rounded-lg shadow flex justify-between">
+                <div>
+                    <h3 class="text-sm font-semibold text-yellow-800 dark:text-gray-100">New Employees This Month</h3>
+                    <p class="text-3xl font-bold text-yellow-700 dark:text-yellow-100">{{ $newEmployeesThisMonth }}</p>
+                </div>
+
+                 <!-- Export to Excel -->
+                 <div class="w-1/5 sm:w-auto flex justify-center items-center h-full">
+                    <button wire:click="exportUsers" 
+                        class="inline-flex items-center focus:outline-none"
+                        type="button" title="Export to Excel">
+                        <img class="flex dark:hidden" src="/images/export-excel.png" width="22" alt="">
+                        <img class="hidden dark:block" src="/images/export-excel-dark.png" width="22" alt="">
+                    </button>
+                </div>
             </div>
-            
+                        
             <!-- Department Distribution Card -->
-            <div class="bg-purple-100 dark:bg-purple-800 p-4 rounded-lg shadow">
-                <h3 class="text-lg font-semibold text-purple-800 dark:text-purple-200">Department Distribution</h3>
+            <div class="col-span-full sm:col-span-6 bg-purple-100 dark:bg-purple-800 p-4 rounded-lg shadow">
+                <h3 class="text-sm font-semibold text-purple-800 dark:text-gray-100">Department Distribution</h3>
                 <ul class="mt-2">
                     @foreach($departmentCounts as $dept)
                         <li class="text-sm text-purple-600 dark:text-purple-300">
-                            {{ $dept->department }}: {{ $dept->count }}
+                            <div class="flex bg-purple-50 dark:bg-purple-900 px-4 py-2 rounded-sm justify-between mb-2">
+                                <div>
+                                    <p class="text-sm text-purple-800 dark:text-purple-200">
+                                        {{ $dept->department }}: {{ $dept->count }}
+                                    </p>
+                                </div>
+
+                                <!-- Export to Excel -->
+                                <div class="w-1/5 sm:w-auto flex justify-center items-center h-full">
+                                    <button wire:click="exportUsers" 
+                                        class="inline-flex items-center focus:outline-none"
+                                        type="button" title="Export to Excel">
+                                        <img class="flex dark:hidden" src="/images/export-excel.png" width="22" alt="">
+                                        <img class="hidden dark:block" src="/images/export-excel-dark.png" width="22" alt="">
+                                    </button>
+                                </div>
+                            </div>
                         </li>
                     @endforeach
                 </ul>
             </div>
+            
+            <!-- Daily Attendance -->
+            <div class="col-span-full sm:col-span-6 bg-teal-50 dark:bg-teal-800 p-4 rounded-lg shadow flex flex-col items-left">
+                <div class="block sm:flex justify-between items-center">
+                    <div class="flex justify-left items-center">
+                        <img class="flex dark:hidden" src="/images/calendar.png" alt="">
+                        <h3 class="text-sm font-semibold text-teal-800 dark:text-gray-100">Daily Attendance</h3>
+                    </div>
+                    <div class="w-full sm:w-auto flex items-center relative">
+                        <input type="date" id="endDate" wire:model.live='date' value="{{ $date }}"
+                        class="px-2 py-1.5 block w-4/5 sm:text-sm border border-teal-800 hover:bg-gray-300 rounded-md 
+                            dark:hover:bg-slate-600
+                            text-teal-800 dark:bg-teal-400">
+
+                         <!-- Export to Excel -->
+                        <div class="flex justify-center items-center w-1/5 h-full ml-2" style="width: 32px; height: 32px;">
+                            <button wire:click="exportUsers" 
+                                class="inline-flex items-center focus:outline-none"
+                                type="button" title="Export to Excel">
+                                <img class="flex dark:hidden" src="/images/export-excel.png" alt="">
+                                <img class="hidden dark:block" src="/images/export-excel-dark.png" alt="">
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-2 sm:mt-0">
+                    <h3 class="text-xs text-teal-800 dark:text-gray-100">Total Present/Late</h3>
+                    <p class="text-3xl font-bold text-teal-700 dark:text-teal-100">{{ $dailyAttendance }}</p>
+                </div>
+                
+            </div>
+
         </div>
     </div>
 
