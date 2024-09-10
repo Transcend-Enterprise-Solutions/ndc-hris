b385b720626d2dd252092d75dcc65a51dca69962
Jhon Francis Duarte <jhamaicaduarte@gmail.com>
2024-09-10 14:11:43 +0800

    add

--- app/Services/BioTimeService.php
+++ app/Services/BioTimeService.php
@@ -13,7 +13,7 @@ class BioTimeService
     {
         $this->client = new Client([
             'headers' => [
-                'Authorization' => 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNzIzNzA0Mjc1LCJlbWFpbCI6Impob25mcmFuY2lzZHVhc3J0ZTEyMzQ1QGdtYWlsLmNvbSIsIm9yaWdfaWF0IjoxNzIzMDk5NDc1fQ.VXoumkVKTqjY0VCnI2IVf8C4WyGZvO33Kp8I9KRAOho',
+                'Authorization' => 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwiZXhwIjoxNzI2NTUzNDAxLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsIm9yaWdfaWF0IjoxNzI1OTQ4NjAxfQ.I2qzfZP3x7VGRLOvb5lt3GybYlSikVSeSA5-4xUwwgc',
                 'Content-Type' => 'application/json',
                 'Accept' => 'application/json',
             ],
