8a6a056130d7dc503ba89daebc116414b923cb8e
Jhon Francis Duarte <jhamaicaduarte@gmail.com>
2024-12-23 18:18:31 +0800

    wfh management added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
     <script defer src="build/assets/app-DEoBNXZR.js"></script>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="build/assets/app-kJkzIgp8.css">
+    <link rel="stylesheet" href="build/assets/app-CqmQecxL.css">
 
     <style>
         [x-cloak] {
--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -21,7 +21,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     
     <!-- Styles -->
-    <link rel="stylesheet" href="build/assets/app-kJkzIgp8.css">
+    <link rel="stylesheet" href="build/assets/app-CqmQecxL.css">
 
     @livewireStyles
     <style>
