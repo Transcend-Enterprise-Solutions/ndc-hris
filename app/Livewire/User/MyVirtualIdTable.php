3b8b7f84bb56d15e2b2cee28889051c0d0b3598d
janllyod09 <bulicjanllyod@gmail.com>
2025-01-18 16:25:52 +0800

    goods id

--- app/Livewire/User/MyVirtualIdTable.php
+++ app/Livewire/User/MyVirtualIdTable.php
@@ -6,9 +6,86 @@ use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Support\Facades\DB;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class MyVirtualIdTable extends Component
 {
+
+    // public function downloadPdf()
+    // {
+    //     $user = Auth::user();
+    //     $userData = $user->userData;
+
+    //     $position = DB::table('positions')
+    //         ->where('id', $user->position_id)
+    //         ->value('position') ?? 'No position assigned';
+
+    //     $formattedDateOfBirth = $userData->date_of_birth 
+    //         ? \Carbon\Carbon::parse($userData->date_of_birth)->format('F j, Y') 
+    //         : 'N/A';
+
+    //     // Get QR code
+    //     $qrCode = $this->qrCode;
+
+    //     // Create PDF in landscape orientation
+    //     $pdf = PDF::loadView('pdf.virtual-id', [
+    //         'name' => $user->name,
+    //         'emp_code' => $user->emp_code,
+    //         'profilePhotoPath' => $user->profile_photo_path 
+    //             ? public_path('storage/' . $user->profile_photo_path) 
+    //             : public_path('default-avatar.png'),
+    //         'dateOfBirth' => $formattedDateOfBirth,
+    //         'placeOfBirth' => $userData->place_of_birth ?? null,
+    //         'sex' => $userData->sex ?? null,
+    //         'civilStatus' => $userData->civil_status ?? null,
+    //         'bloodType' => $userData->blood_type ?? null,
+    //         'position' => $position,
+    //         'qrCode' => $qrCode
+    //     ]); // Set the paper size to A4 in landscape orientation
+
+    //     return response()->streamDownload(
+    //         fn () => print($pdf->output()),
+    //         "virtual-id.pdf"
+    //     );
+    // }
+    public function downloadPdf()
+    {
+        $user = Auth::user();
+        $userData = $user->userData;
+
+        $position = DB::table('positions')
+            ->where('id', $user->position_id)
+            ->value('position') ?? 'No position assigned';
+
+        $formattedDateOfBirth = $userData->date_of_birth 
+            ? \Carbon\Carbon::parse($userData->date_of_birth)->format('F j, Y') 
+            : 'N/A';
+
+        // Get QR code as base64 string
+        $qrCode = base64_encode($this->qrCode);
+
+        // Create PDF in landscape orientation
+        $pdf = PDF::loadView('pdf.virtual-id', [
+            'name' => $user->name,
+            'emp_code' => $user->emp_code,
+            'profilePhotoPath' => $user->profile_photo_path 
+                ? public_path('storage/' . $user->profile_photo_path) 
+                : public_path('default-avatar.png'),
+            'dateOfBirth' => $formattedDateOfBirth,
+            'placeOfBirth' => $userData->place_of_birth ?? null,
+            'sex' => $userData->sex ?? null,
+            'civilStatus' => $userData->civil_status ?? null,
+            'bloodType' => $userData->blood_type ?? null,
+            'position' => $position,
+            'qrCode' => $qrCode  // Pass the base64-encoded QR code
+        ]);
+
+        return response()->streamDownload(
+            fn () => print($pdf->output()),
+            "virtual-id.pdf"
+        );
+    }
+
 
     public function getQrCodeProperty()
     {
--- resources/views/livewire/user/my-virtual-id-table.blade.php
+++ resources/views/livewire/user/my-virtual-id-table.blade.php
@@ -4,14 +4,18 @@
             <h1 class="text-lg font-bold text-center text-slate-800 dark:text-white">VIRTUAL ID</h1>
         </div>
 
-        <div class="flex justify-evenly w-full bg-white rounded-xl p-6 dark:bg-gray-800">
+        <button wire:click="downloadPdf" class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-1">
+            Print ID Card
+        </button>
+
+        <div class="flex flex-col md:flex-row justify-evenly gap-4 w-full bg-white rounded-xl p-6 dark:bg-gray-800">
             <!-- Virtual ID Card Front -->
-            <div class="flex flex-col items-center p-4 w-full max-w-lg h-72 bg-slate-200 dark:bg-slate-200 rounded-lg bg-cover bg-center"
-                style="background-image: url('/images/Vector.png'); padding: bottom -4rem;">
+            <div class="flex flex-col items-center p-4 w-full max-w-lg h-48 bg-slate-200 dark:bg-slate-200 rounded-lg bg-cover bg-center md:h-54 md:max-w-lg sm:h-72 sm:max-w-sm"
+                style="background-image: url('/images/Vector.png');">
                 <!-- ID Card Header -->
-                <div class="flex items-center justify-center w-full">
-                    <img src="/images/ndc_logo.png" class="h-12" alt="">
-                    <h2 class="text-xl font-bold">
+                <div class="flex items-center justify-evenly w-full sm:justify-center">
+                    <img src="/images/ndc_logo.png" class="h-4 sm:h-12" alt="">
+                    <h2 class="text-xs font-bold whitespace-nowrap sm:text-xl md:text-md">
                         <span class="text-blue-500">NATIONAL</span>
                         <span class="text-green-500">DEVELOPMENT</span>
                         <span class="text-orange-500">COMPANY</span>
@@ -21,13 +25,13 @@
                 <div class="flex justify-evenly items-center w-full">
                     <div class="mt-4 mb-4 flex flex-col justify-center items-center text-black">
                         <img src="{{ $profilePhotoPath ? asset('storage/' . $profilePhotoPath) : asset('default-avatar.png') }}"
-                            alt="Profile Photo" class="w-32 h-32 border-2 border-white shadow-md object-cover mx-auto">
-                        <p class="text-xs mt-1">{{ $position }}</p>
+                            alt="Profile Photo"
+                            class="w-20 h-20 sm:w-28 sm:h-28 border-2 border-white shadow-md object-cover">
+                        <p class="text-[10px] sm:text-xs mt-1">{{ $position }}</p>
                     </div>
-
                     <div class="flex flex-col items-center text-black">
                         <!-- Personal Details -->
-                        <div class="text-black text-left w-full text-sm">
+                        <div class="text-black text-left w-full text-[10px] sm:text-sm">
                             <p><strong>Name:</strong> {{ $name }}</p>
                             <p><strong>Employee Code:</strong> {{ $emp_code }}</p>
                             <p><strong>Date of Birth:</strong> {{ $dateOfBirth }}</p>
@@ -37,22 +41,21 @@
                             <p><strong>Blood Type:</strong> {{ $bloodType }}</p>
                         </div>
                     </div>
-
                 </div>
                 <!-- Footer -->
-                <div class="w-full flex justify-center items-center text-white text-xs">
-                    <p class="text-center">Issued by NDC</p>
+                <div class="w-full flex justify-center items-center text-white text-[10px] sm:text-xs">
+                    <p class="text-center sm:mt-4">Issued by NDC</p>
                 </div>
             </div>
 
             <!-- Virtual ID Card Back -->
-            <div class="flex flex-col justify-center items-center p-4 w-full h-72 max-w-lg bg-slate-200 dark:bg-slate-200 rounded-lg bg-cover bg-center"
+            <div class="flex flex-col justify-center items-center p-4 w-full max-w-lg h-48 bg-slate-200 dark:bg-slate-200 rounded-lg bg-cover bg-center md:h-54 md:max-w-lg sm:h-72 sm:max-w-sm"
                 style="background-image: url('/images/Vector.png');">
                 <!-- QR Code Display -->
                 <div class="flex justify-center items-center ml-4">
                     <div class="flex justify-center items-start text-black">
                         <!-- Terms and Conditions -->
-                        <div class="text-black text-left w-full text-sm">
+                        <div class="text-black text-left w-full text-[10px] sm:text-sm">
                             <h3 class="font-bold mb-2">TERMS AND CONDITIONS:</h3>
                             <ul class="list-disc pl-4">
                                 <li>This ID is valid for official purposes only.</li>
@@ -61,14 +64,14 @@
                             </ul>
                         </div>
                     </div>
-                    <div class="w-52 h-40 border border-black overflow-hidden">
-                        <div class="w-full h-full flex items-center justify-center">
+                    <div class="sm:w-40 sm:h-32 w-28 h-24 overflow-hidden">
+                        <div class="w-full h-full flex p-1 items-center justify-center">
                             {!! $qrCode !!}
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
+
     </div>
 </div>
