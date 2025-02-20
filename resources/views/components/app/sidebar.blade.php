fd6fd69dfcf1049518ec60655aad5dd3db8b1f29
Jhon Francis Duarte <jhamaicaduarte@gmail.com>
2025-02-20 08:57:13 +0800

    fixed path

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -188,7 +188,7 @@
                                                     </span>
                                                 </a>
                                             </li>
-                                            <li class="mb-1 last:mb-0">
+                                            {{-- <li class="mb-1 last:mb-0">
                                                 <a class="block text-slate-400 hover:text-blue-500 transition duration-150 truncate @if (Route::is('/employee-management/bir2316')) {{ '!text-blue-500' }} @endif"
                                                     href="{{ route('/employee-management/bir2316') }}">
                                                     <span class="text-sm font-medium transition-opacity duration-300"
@@ -197,7 +197,7 @@
                                                         BIR 2316
                                                     </span>
                                                 </a>
-                                            </li>
+                                            </li> --}}
                                         @endif
                                     </ul>
                                 </div>
